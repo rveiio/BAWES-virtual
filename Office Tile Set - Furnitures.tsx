cfbd5609cf27763101f273e7310889f8d60a0a31
rveiio <reem.hasib@bawes.net>
2025-02-11 12:35:47 +0300

    Removed all furnitures
    just testing

---
+++ Office Tile Set - Furnitures.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Office Tile Set - Furnitures" tilewidth="32" tileheight="32" tilecount="848" columns="16">
+ <image source="../../../Downloads/Modern_Office_Revamped_v1.2/2_Modern_Office_Black_Shadow/Modern_Office_Black_Shadow_32x32.png" width="512" height="1696"/>
+</tileset>
---
+++ Office Tile Set - Room Builder.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Office Tile Set - Room Builder" tilewidth="32" tileheight="32" tilecount="224" columns="16">
+ <image source="../../../Downloads/Modern_Office_Revamped_v1.2/1_Room_Builder_Office/Room_Builder_Office_32x32.png" width="512" height="448"/>
+</tileset>
