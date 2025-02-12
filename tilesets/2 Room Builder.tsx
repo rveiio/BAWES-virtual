959f534b2a142473ea9e1a486c00d2ea565ca8f0
rveiio <reem.hasib@bawes.net>
2025-02-12 21:47:55 +0300

    Added all tilesets
    Added all external tilesets needed

---
+++ tilesets/2 Room Builder.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="2 Room Builder" tilewidth="32" tileheight="32" tilecount="224" columns="16">
+ <image source="Room_Builder_Office_32x32.png" width="512" height="448"/>
+</tileset>
