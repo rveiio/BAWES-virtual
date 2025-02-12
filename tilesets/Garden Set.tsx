016cb1a9bce4bb51c68772ccb6df7e76dd1143bc
rveiio <reem.hasib@bawes.net>
2025-02-12 12:16:22 +0300

    added garden tilesets
    added grass and some rocks

---
+++ tilesets/Garden Set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Garden Set" tilewidth="32" tileheight="32" tilecount="1596" columns="42">
+ <image source="_free__tileset_v0_1_8_by_chaoticcherrycake_ddpe9c3_scaled_2x_pngcrushed.png" width="1344" height="1216"/>
+</tileset>
