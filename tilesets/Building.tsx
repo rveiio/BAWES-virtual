c7ce2be605efbe6b3037121cf21c1119e0b2ed3c
rveiio <reem.hasib@bawes.net>
2025-02-13 08:51:09 +0300

    Photoshopped tileset
    testing if changing the tileset image works

---
+++ tilesets/Building.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Building" tilewidth="32" tileheight="32" tilecount="703" columns="37">
+ <image source="snow_and_stone_by_magiscarf_dcjgz10_scaled_2x_pngcrushed.png" width="1184" height="608"/>
+</tileset>
