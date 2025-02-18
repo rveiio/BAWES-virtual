a00291b8ebf9c13e4e3be95cf0845e715d5facc0
rveiio <reem.hasib@bawes.net>
2025-02-18 11:07:00 +0300

    Create Fishing.tsx

---
+++ tilesets/Fishing.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Fishing" tilewidth="32" tileheight="32" tilecount="432" columns="16">
+ <image source="9_Fishing_Black_Shadow_32x32.png" width="512" height="864"/>
+</tileset>
