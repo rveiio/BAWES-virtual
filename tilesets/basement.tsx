26682843ed04f914f52804d68c4412bd4938df8c
rveiio <reem.hasib@bawes.net>
2025-02-18 11:05:29 +0300

    More tiles and Furnitures
    furnished inner circle and added more tiles

---
+++ tilesets/basement.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="basement" tilewidth="32" tileheight="32" tilecount="800" columns="16">
+ <image source="14_Basement_Black_Shadow_32x32.png" width="512" height="1600"/>
+</tileset>
---
+++ tilesets/music.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="music" tilewidth="32" tileheight="32" tilecount="768" columns="16">
+ <image source="6_Music_and_sport_Black_Shadow_32x32.png" width="512" height="1536"/>
+</tileset>
