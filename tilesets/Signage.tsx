7ed2373728092cb1eb8dac0be14a40810894a416
rveiio <reem.hasib@bawes.net>
2025-03-02 14:02:31 +0300

    Signage and Spawn Point
    Added Signs and Re adjusted spawn point

---
+++ tilesets/Signage.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Signage" tilewidth="32" tileheight="32" tilecount="200" columns="10">
+ <image source="Signage.png" width="320" height="640"/>
+</tileset>
