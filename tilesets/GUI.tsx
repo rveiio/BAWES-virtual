039780f4ac7d2669447842b4003bedcb68413867
rveiio <reem.hasib@bawes.net>
2025-03-04 13:13:02 +0300

    Map Designers Section
    Changed From Notion form to Google Form

---
+++ tilesets/GUI.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="GUI" tilewidth="32" tileheight="32" tilecount="828" columns="46">
+ <image source="Spritesheet_UI_Flat.png" width="1472" height="576"/>
+ <tile id="240">
+  <animation>
+   <frame tileid="240" duration="300"/>
+   <frame tileid="241" duration="300"/>
+   <frame tileid="242" duration="300"/>
+   <frame tileid="241" duration="300"/>
+   <frame tileid="240" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="282">
+  <animation>
+   <frame tileid="282" duration="300"/>
+   <frame tileid="283" duration="300"/>
+   <frame tileid="284" duration="300"/>
+   <frame tileid="285" duration="300"/>
+   <frame tileid="284" duration="300"/>
+   <frame tileid="283" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
